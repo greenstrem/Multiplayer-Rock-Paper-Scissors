a9e521c67b3d7672390879059556e5ece7cdf6da
fycom <fycom@live.ru>
2025-01-11 16:55:51 +0300

    Читерская игра. Пека всегда вин

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+
+
+def comp_choice():
+    choices = ["камень", "ножницы", "бумага"]
+    return random.choice(choices)
+
+def ask_user():
+    x = input(f"Камень, ножницы или бумага?\n").lower()
+    while x not in ["камень", "ножницы", "бумага"]:
+        print("Выбери из представленного")
+        x = input(f"Камень, ножницы или бумага?\n").lower()
+    return x
+
+def sanya(pc, user):
+    # if pc == user:
+    #     print("Ничья")
+    rules = {
+        "камень":"ножницы",
+        "ножницы":"бумага",
+        "бумага":"камень"
+    }
+    if pc == user:
+        print("Ничья")
+    elif rules[user] == pc:
+        print("Ты победил!")
+    else:
+        print('Ты проиграл!')
+
+def game():
+    pc = comp_choice()
+    user = ask_user()
+    print(f'Компуктер выбрал {pc}\nа ты выбрал {user}')
+    sanya(pc = pc, user = user)
+
+game()
